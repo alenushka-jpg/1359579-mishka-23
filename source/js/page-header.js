50fc2118a774bed1c84230e024443a296878dac9
alenushka <alionxlo@gmail.com>
2021-09-12 18:24:29 +0300

    Делает меню в хедере, добавляет нерабочий скрипт
    Исправляет название картинок, добавляет декоративные элементы и центрирует

---
+++ source/js/page-header.js
@@ -0,0 +1,18 @@
+const pageHeader = document.querySelector('.page-header');
+const burger = document.querySelector('.page-header__burger');
+
+pageHeader.classList.remove('page-header--nojs');
+
+burger.addEventListener('clock', function() {
+  if (pageHeader.classList.contains('page-header--closed')) {
+    pageHeader.classList.remove('page-header--closed');
+    pageHeader.classList.add('page-header--opened');
+    burger.classList.remove('page-header__burger--opened');
+    burger.classList.add('page-header__burger--closed');
+  } else {
+    pageHeader.classList.add('page-header--closed');
+    pageHeader.classList.remove('page-header--opened');
+    burger.classList.add('page-header__burger--opened');
+    burger.classList.remove('page-header__burger--closed');
+  }
+});
