c9a4e1fbec598dba44d575e85aa9d496674699f5
alenushka <alionxlo@gmail.com>
2021-09-15 20:29:59 +0300

    Последние изменения в проекте

--- gulpfile.js
+++ gulpfile.js
@@ -142,7 +142,7 @@ const reload = (done) => {
 // Watcher
 
 const watcher = () => {
-  gulp.watch("source/sass/**/*.sass", gulp.series(styles));
+  gulp.watch("source/sass/**/*.scss", gulp.series(styles));
   gulp.watch("source/js/*.js", gulp.series(scripts));
   gulp.watch("source/*.html", gulp.series(html, reload));
 }
